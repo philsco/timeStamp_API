4e453f480ddcad341f7d159308cc0c41469381cf
philsco <philsco@cloudshiftit.com>
2016-01-06 15:58:48 -0800

    Port number based on ENV

--- timestamp.js
+++ timestamp.js
@@ -48,9 +48,8 @@ app.all('/*', function (req, res) {
     res.send(payload());
 });
 
-var server = app.listen(3000, function () {
+  var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
-//  var port = server.address().port;
-  var port = process.env.PORT || 3000;
+  var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
